c5b6c97936acb75eb52cb898e96f523d71932a46
HaveACigar <ariedekraker@gmail.com>
2021-10-08 08:58:27 -0400

    Make beyondBurger change from open to closed or vice versa

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Topbar from "./components/topbar/Topbar"
 import Works from "./components/works/Works";
 import "./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(true)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
