0a01c50c8308a8e9f03c9452149e0e44244f7dff
HaveACigar <ariedekraker@gmail.com>
2021-10-07 22:59:41 -0400

    Add icons for topbar and format it better

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,20 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
-                <div className="left" cl>
-                    <a href="#intro"></a>
+                <div className="left">
+                    <a href="#intro">Click me</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+21 53 12 65</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail/>
+                        <span>testAccount@hotmail.com</span>
+                    </div>
                 </div>
                 <div className="right">
                 </div>
