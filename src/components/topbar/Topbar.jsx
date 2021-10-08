aa2ecda655c8b068db7c1611035a787d5a894138
HaveACigar <ariedekraker@gmail.com>
2021-10-07 22:27:49 -0400

    Make several changes to change the style of different components in the website

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left" cl>
+                    <a href="#intro"></a>
+                </div>
+                <div className="right">
+                </div>
+            </div>            
+        </div>
+    )
+}
