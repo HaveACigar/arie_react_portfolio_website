3ff653af62fc567e64de14293f0d79badadf38f5
HaveACigar <ariedekraker@gmail.com>
2021-10-08 17:57:03 -0400

    finish creating animation for intro and start work on portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useRef } from "react"
+import { init } from "ityped";
 import "./intro.scss"
 
 export default function Intro() {
+
+    const textRef = useRef();
+    useEffect(()=>{
+        init(textRef.current, { 
+            showCursor: false,
+            backDelay: 1500,
+            backSpeed: 60,
+            showCursor: true, 
+            strings: ['website!', 'workshop!' , 'playground!'] 
+        });
+    }, []);
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,10 +25,10 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hello! My name is</h2>
                     <h1>Arie DeKraker</h1>
-                    <h3>and welcome to my website!<span></span></h3>
+                    <h3>and welcome to my <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
-                    <img src="assets/" alt="" />
+                    <img src="assets/white_downarrow.png" alt="click here to move down!"/>
                 </a>
             </div>
             
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Java from EMU</li>
+                <li>Webdesign from EMU</li>
+                <li>Current projects</li>
+                <li>Ideas</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/EMU.png" alt="" />
+                    <h3>EMU</h3>
+                </div>
+            </div>
         </div>
     )
 }
