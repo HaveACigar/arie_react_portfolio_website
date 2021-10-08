0624e6584ea7a5066cf7d6d161954ea7cc7987e3
HaveACigar <ariedekraker@gmail.com>
2021-10-08 17:05:47 -0400

    fill out the intro area and include pictures

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/Arie_ProfilePic.jpg" alt="A picture of me" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello! My name is</h2>
+                    <h1>Arie DeKraker</h1>
+                    <h3>and welcome to my website!<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/" alt="" />
+                </a>
+            </div>
             
         </div>
     )
