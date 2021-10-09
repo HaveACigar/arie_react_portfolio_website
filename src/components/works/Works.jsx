e63f82ca7ab2a3aa15220857d389e6a9f426cd3d
HaveACigar <ariedekraker@gmail.com>
2021-10-09 18:31:16 -0400

    start formating the works page

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,25 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>
+                                    dsafdfasdfadsfadfasdfasf
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
