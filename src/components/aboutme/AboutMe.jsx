2d0f759d6cb7bebc1eac730476cffc1bd30da6a6
HaveACigar <adekrake@emich.edu>
2021-10-24 23:38:59 -0400

    add images to the aboutMe page

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -3,7 +3,17 @@ import "./aboutMe.scss"
 export default function AboutMe() {
     return (
         <div className="aboutMe">
-            
+            <div className="left">
+                <div className="card-background"></div>
+                <div className="card">
+                    <img src="assets/java.png" alt="" />
+                    <img src="assets/react.png" alt="" />
+                    <img src="assets/python.png" alt="" />
+                    <img src="assets/nodejs.png" alt="" />
+                    <img src="assets/angular.png" alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
