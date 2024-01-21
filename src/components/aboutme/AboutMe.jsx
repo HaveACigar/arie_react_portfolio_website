284d6d365a2f2a64fd61828362f18473767c5ec7
Arie DeKraker <ariedekraker@gmail.com>
2024-01-21 11:54:17 -0700

    Update profile picture and about me page

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -13,9 +13,10 @@ export default function AboutMe() {
             </div>
             <div className="right">
                 <h1>About me</h1>
-                <p>I am a software engineer working at Ford Motor Company. I obtained a major in computer science and a minor in mathematics at Eastern Michigan University. I am looking for a position that would allow me to utilize and build on my programming skills.</p>
+                <p>Hello! I'm a data-driven professional with a strong background in software engineering and an evolving passion for data science. With a Bachelor's degree in Computer Science from Eastern Michigan University and ongoing Master's studies in Data Science and Analytics, I blend technical expertise with analytical acumen.</p>
                 <br />
-                <p className="update">I use this page to display my work as well as work on my web design skills. Some of the pages are work in progress. I plan on trying to add several updates throughout the week, but might miss days occasionally. You can view progress updates by viewing the commits on my github page. (link to it at the top)</p>
+                <p className="update">
+                    On this page, you'll discover a curated collection of both academic and personal projects that I have undertaken and successfully completed in the past. These projects showcase my skills and interests, reflecting my journey in the realms of software engineering and data science.</p>
             </div>
         </div>
     )
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,7 +21,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/Arie_ProfilePic.PNG" alt="A picture of me" />
+                    <img src="assets/Arie_Profile_Pic.jpg" alt="A picture of me" />
                 </div>
             </div>
             <div className="right">
